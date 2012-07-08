d0688950df4c9e7fbe6b213aae33b8d85af8ea8d
keyvankardel <keyvankardel@gmail.com>
2012-07-08 15:33:12 +0000

    new updates to some DAOs

--- jsp/src/main/java/de/tum/in/dbpra/model/dao/FlightSegmentDAO.java
+++ jsp/src/main/java/de/tum/in/dbpra/model/dao/FlightSegmentDAO.java
@@ -3,8 +3,10 @@ package de.tum.in.dbpra.model.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.util.LinkedList;
 import java.util.List;
 
 import de.tum.in.dbpra.model.bean.FlightSegmentBean;
@@ -33,12 +35,59 @@ public class FlightSegmentDAO extends AbstractDAO{
 		}
 	}
 	
-	public List<FlightSegmentBean> findSegsForPotentialFlight(List<RoutePair> routePairs){
-		//TODO
-		return null;
+	public List<FlightSegmentBean> findSegsForPotentialFlight(List<RoutePair> routePairs) 
+			throws FlightSegmentNotFoundException{
+		
+		String query = new StringBuilder()
+		.append("SELECT FLIGHTNR, ARRIVAL_TIME, ARRIVAL_DATE, DEPARTURE_TIME, DEPARTURE_DATE, ROUTEID ")
+		.append("FROM FLIGHTSEGMENT f1, FLIGHTSEGMENT f2 ")
+		.append("WHERE ")
+		.toString();
+		
+		for (RoutePair routePair : routePairs) {
+			query.concat("(");
+			query.concat("f1.ROUTEID = " + routePair.getFirstRouteID() + " AND " + 
+					"f2.ROUTEID = " + routePair.getSecondRouteID());
+			query.concat(") ");
+			if((routePairs.size() > 1) && (routePairs.indexOf(routePair) < (routePairs.size() - 1))){
+				query.concat(" OR ");
+			}
+		}
+		
+		List<FlightSegmentBean> flightSegs = new LinkedList<FlightSegmentBean>();
+		
+		try (Connection connection = getConnection();
+				 PreparedStatement preparedStatement = connection.prepareStatement(query);) {
+			
+			try (ResultSet resultSet = preparedStatement.executeQuery();) {
+				while (resultSet.next()) {
+					FlightSegmentBean flightSeg = new FlightSegmentBean();
+					flightSeg.setFilghtNr(resultSet.getInt(1));
+					flightSeg.setArrivalTime(resultSet.getTime(2));
+					flightSeg.setArrivalDate(resultSet.getDate(3));
+					flightSeg.setDepartureTime(resultSet.getTime(4));
+					flightSeg.setDepartureDate(resultSet.getDate(5));
+					flightSeg.setRouteId(resultSet.getInt(6));
+					flightSegs.add(flightSeg);
+				}
+				resultSet.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new FlightSegmentNotFoundException();
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new FlightSegmentNotFoundException();
+		}
+		return flightSegs;
 	}
 	
 	@SuppressWarnings("serial")
 	public class FlightSegmentInsertException extends Throwable{
+	}
+	
+	@SuppressWarnings("serial")
+	public class FlightSegmentNotFoundException extends Throwable{
 	}
 }
