eb42a59ca521105416aa993d603772c0183c8fc9
ankit.9644 <ankit.9644@gmail.com>
2012-07-08 11:15:35 +0000

    Beans and DAO related to Airport and Route

---
+++ jsp/src/main/java/de/tum/in/dbpra/model/dao/RouteDAO.java
@@ -0,0 +1,21 @@
+package de.tum.in.dbpra.model.dao;
+
+import java.util.List;
+
+import de.tum.in.dbpra.model.bean.RoutePairBean;
+
+public class RouteDAO {
+	
+	public int getRouteID(String apcodeSource, String apcodeDestination) {
+		
+		
+		return 0;
+		
+	}
+	
+	public List<RoutePairBean> getRoutePairs(String apcodeSource, String apcodeDestination) {
+		return null;
+		
+	}
+
+}
