52240f0a79a842aa1f8e2025087cb0f10f94c30c
ankit.9644@gmail.com <ankit.9644@gmail.com@8076589f-035f-3b0c-3dd3-46e92e7063b9>
2012-07-25 08:43:38 +0000

    updated from previous apache project

--- FinalDb/WebContent/home1/jS/main.js
+++ FinalDb/WebContent/home1/jS/main.js
@@ -1,3 +1,4 @@
 document.write("<script type="text/javascript"  src="jquery-1.6.4.min.js"  charset="${_response_encoding}"></script>");
 document.write("<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>");
 document.write("<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>");
+	
